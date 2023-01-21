b4ba14acb7792c998985053d349e0fe5f982cbda
60jong <rudwhd515@gmail.com>
2023-01-22 02:02:50 +0900

    feat : 이벤트 시작 날짜와 종료 날짜를 담는 Period 객체 생성

---
+++ src/main/java/com/app/everyvent/domain/Period.java
@@ -0,0 +1,13 @@
+package com.app.everyvent.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class Period {
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
