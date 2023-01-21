e58e7ff48a611745a291699a98ab1f8acba5003a
60jong <rudwhd515@gmail.com>
2023-01-22 02:04:16 +0900

    refactor : JejuAir 객체 리팩터링

--- src/main/java/com/app/everyvent/domain/airline/JejuAir.java
+++ src/main/java/com/app/everyvent/domain/airline/JejuAir.java
@@ -1,12 +1,11 @@
 package com.app.everyvent.domain.airline;
 
 import com.app.everyvent.domain.Event;
+import com.app.everyvent.domain.Period;
 import lombok.NoArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
@@ -18,8 +17,6 @@ import java.util.List;
 @DiscriminatorValue("JJA")
 @NoArgsConstructor
 public class JejuAir extends Airline {
-    public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
-    public static String WEB_DRIVER_PATH = "chromedriver.exe";
     private static final String JJA_EVENT_URL = "https://www.jejuair.net/ko/event/event.do";
 
     public JejuAir(String koreanName, String englishName, String country) {
@@ -27,65 +24,75 @@ public class JejuAir extends Airline {
     }
 
     @Override
-    public List<Event> crawlEvents() {
-        ChromeOptions options = crawlConfig();
+    public List<Event> crawlEvents() throws InterruptedException {
+        WebDriver driver = super.getWebDriver();
+        driver.get(JJA_EVENT_URL);
 
-        WebDriver driver = new ChromeDriver(options);
+        super.waitPageLoad();
 
+        clickMoreButtons(driver);
+
+
+        List<Event> events = getEvents(driver);
+
+        driver.close();
+
+        return events;
+    }
+
+    private void clickMoreButtons(WebDriver driver) {
+        while (true) {
+            try {
+                WebElement more__button = driver.findElement(By.className("more__button"));
+                more__button.click();
+                super.waitPageLoad();
+            } catch (Exception e) {
+                break;
+            }
+        }
+    }
+
+    public List<Event> getEvents(WebDriver driver) {
         List<Event> events = new ArrayList<>();
-        try {
-            driver.get(JJA_EVENT_URL);
-            Thread.sleep(2000); // 3. 페이지 로딩 대기 시간
 
-            // 4. 더보기 버튼 클릭
-            while (true) {
-                try {
-                    WebElement more__button = driver.findElement(By.className("more__button"));
-                    more__button.click();
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                    break;
-                }
-            }
+        List<WebElement> eventElements = driver.findElements(By.className("search-result__item"));
 
-            // 이벤트 선택
-            List<WebElement> elements = driver.findElements(By.className("search-result__item"));
-
-            for (WebElement element : elements) {
-                String eventUrl = element.findElement(By.tagName("a")).getAttribute("href");
-                String eventText = getEventText(element);
-                String period = element.findElement(By.className("event-banner__date")).getText();
-
-                LocalDate startDate = LocalDate.parse(period.split("~")[0].trim().replaceAll("[.]","-"));
-                LocalDate endDate = LocalDate.parse(period.split("~")[1].trim().replaceAll("[.]","-"));
-
-                events.add(new Event(this, eventUrl, eventText, startDate, endDate));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            driver.close(); // 5. 브라우저 종료
+        for (WebElement eventElement : eventElements) {
+            events.add(makeEvent(eventElement));
         }
 
         return events;
     }
 
-    private ChromeOptions crawlConfig() {
-        // WebDriver 경로 설정
-        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+    public Event makeEvent(WebElement element) {
+        String eventUrl = element.findElement(By.tagName("a")).getAttribute("href");
+        String eventText = getEventText(element);
+        Period period = getPeriod(element);
 
-        // WebDriver 옵션 설정
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-popup-blocking");
-
-        return options;
+        return new Event(this, eventUrl, eventText, period.getStartDate(), period.getEndDate());
     }
 
-    private String getEventText(WebElement e) {
-        String eventText = e.findElement(By.className("event-banner__text")).getText();
-        String eventTitle = e.findElement(By.className("event-banner__title")).getText();
+    public String getEventText(WebElement element) {
+        String eventText = element.findElement(By.className("event-banner__text")).getText();
+        String eventTitle = element.findElement(By.className("event-banner__title")).getText();
         return eventText + " " + eventTitle;
+    }
+
+    public Period getPeriod(WebElement element) {
+        // "yyyy.MM.dd ~ yyyy.MM.dd" 형식
+        String period = element.findElement(By.className("event-banner__date")).getText();
+
+        LocalDate startDate = LocalDate.parse(
+                period.split("~")[0]
+                        .trim()
+                        .replaceAll("[.]","-"));
+
+        LocalDate endDate = LocalDate.parse(
+                period.split("~")[1]
+                        .trim()
+                        .replaceAll("[.]","-"));
+
+        return new Period(startDate, endDate);
     }
 
 }
