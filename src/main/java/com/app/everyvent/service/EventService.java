ed8cc4a567df852043e3da280ccc68ea9c1b3da3
60jong <rudwhd515@gmail.com>
2023-01-27 15:27:39 +0900

    feat : 이벤트 크롤링 후, 이벤트의 type을 check하는 기능 구현

--- src/main/java/com/app/everyvent/service/EventService.java
+++ src/main/java/com/app/everyvent/service/EventService.java
@@ -1,12 +1,14 @@
 package com.app.everyvent.service;
 
 import com.app.everyvent.domain.Event;
+import com.app.everyvent.domain.EventType;
 import com.app.everyvent.domain.airline.Airline;
 import com.app.everyvent.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -14,25 +16,39 @@ import java.util.List;
 @Transactional
 public class EventService {
     private final EventRepository eventRepository;
+    private final DestinationService destinationService;
 
     public void save(Event event) {
         eventRepository.save(event);
     }
 
     public int crawl(Airline airline) {
-        List<Event> events = null;
+        List<Event> events = Collections.emptyList();
+
         try {
             events = airline.crawlEvents();
+            events.stream()
+                    .forEach(event -> setEventType(event));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return saveNew(events);
     }
 
+    public void setEventType(Event event) {
+        EventType eventType = EventType.ETC;
+
+        if (destinationService.canFindDestinationIn(event.getText())) {
+            eventType = EventType.TRAVEL;
+        }
+
+        event.setEventType(eventType);
+    }
+
     public int saveNew(List<Event> events) {
         int newEvents = 0;
         for (Event event : events) {
-            if (!isNew(event)) {
+            if (has(event)) {
                 continue;
             }
             save(event);
@@ -42,11 +58,11 @@ public class EventService {
         return newEvents;
     }
 
-    public Boolean isNew(Event event) {
+    public boolean has(Event event) {
         if (eventRepository.existsByUrl(event.getUrl())) {
-            return false;
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
