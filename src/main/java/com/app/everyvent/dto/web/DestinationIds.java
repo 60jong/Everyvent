917a5808ebccaf57065aae90fb4a74285e25e0da
60jong <rudwhd515@gmail.com>
2023-02-02 16:37:17 +0900

    feat : Destination의 id들을 받는 dto 생성

---
+++ src/main/java/com/app/everyvent/dto/web/DestinationIds.java
@@ -0,0 +1,14 @@
+package com.app.everyvent.dto.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DestinationIds {
+    private List<Long> destinationIds;
+}
