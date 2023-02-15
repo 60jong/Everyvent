aede48ea5fc81e63f80dae45cff8cb883558f084
60jong <rudwhd515@gmail.com>
2023-02-15 18:04:10 +0900

    feat : Flyvent 웹 페이지 (메인 페이지, 회원가입 페이지 구현)

---
+++ src/main/java/com/app/flyvent/dto/web/MemberSigninParam.java
@@ -0,0 +1,11 @@
+package com.app.flyvent.dto.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberSigninParam {
+    private String email;
+    private String password;
+}
